1490b7ba99c7610cbb78a4a2255b03eda375874b
Mohinish Sharma <sharmamohinish67@gmail.com>
2018-12-22 15:41:56 +0530

    Added repo

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    /**
+     * Created to ease the process of displaying table in html
+     * 
+     * @author Mohinish Sharma
+     * @package mHelp
+     * @version v1.1.2
+     */
+    require('inc/mHelp.php');
+    use mHelp\mHelp as mHelp;
+
+    // database connection
+    $d = array(
+        "hostname"=>"localhost",
+        "port"=>"3307", // port is opitonal and not used in this implementation
+        "username"=>"root",
+        "password"=>"",
+        "database"=>"edusearch"
+    );
+
+    // Coulmn name and Column display name mapping 
+    // => with key as column name and value as column display name
+    $c = array(
+        "id"=>"#",
+        "name"=>"Paper name",
+        "description"=>"Description",
+        "dtimes"=>"No. of Downloads",
+        "type"=>"Type"
+    );
+
+    $v = new mHelp($d); // mhelp object
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?= $v->make_table("softwares",$c)?>
+</body>
+</html>
